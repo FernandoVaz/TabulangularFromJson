cf11b100a0aa148cd24ec8d113cd0c55eb6ba6fd
Fernando Vaz <contact@fernandomvaz.com>
2020-11-03 15:30:51 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+//import studentsData from './students.json';
+
+interface Student {
+    id: Number;
+    name: String;
+    email: String;
+    gender: String;
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'tabulangularFromJson';
+
+export class AppComponent  {
+  title = "tabulangularFromJson";
+  name = 'Angular';
+  //students: Student[] = studentsData;
 }
